600192475ea95b5212f62d673e03b2a771709684
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-10-09 00:12:25 +0530

    this is bash commit

---
+++ JS/arrowf1.js
@@ -0,0 +1,10 @@
+
+ console.log("Running");
+// const greet =(name,end) => "Good Morning " + name+" "+ end;
+
+// console.log (greet("Alice", "bye"));
+
+// Arrow functions
+// const greet = (name,end)=> "Good Morning "+name+" "+ end;
+
+// console.log(greet("Alice", "bye"));
---
+++ JS/thisarrow.js
@@ -0,0 +1,8 @@
+
+const cameras = {
+    price:      600,
+    weight:     1000,
+    myDesc:     function(){
+                    return `This canon camera is off ${this.price}$`
+                    }
+}
